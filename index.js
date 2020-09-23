e250b557edfdfe402ed646ebd6204705a31cc20b
Vinh Nguyen <xuanvinhct@gmail.com>
2020-09-23 11:36:48 +0900

    Fix bug

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const path = require('path');
 
-try {
-  const time = (new Date()).toTimeString();
-  const files = JSON.parse(core.getInput('files'));
+async function main(){
+  try {
+    const time = (new Date()).toTimeString();
+    const files = JSON.parse(core.getInput('files'));
 
-  const src = core.getInput('src');
-  const dest = core.getInput('dest');
-  const ftpUsername = core.getInput('ftpUsername');
-  const ftpPassword = core.getInput('ftpPassword');
-  const ftpHostname = core.getInput('ftpHostname');
+    const src = core.getInput('src');
+    const dest = core.getInput('dest');
+    const ftpUsername = core.getInput('ftpUsername');
+    const ftpPassword = core.getInput('ftpPassword');
+    const ftpHostname = core.getInput('ftpHostname');
 
-  files.forEach(async (file) => {
-    if (!file.filename.startsWith(src))
-      return;
-    const remoteFilePath = file.filename.substr(src.length);
-    const remoteDirPath = path.dirname(remoteFilePath);
-    const serverPath = `${ftpHostname}/${dest}/${remoteDirPath}/`.replace(/\/\//g, '/');
-    const fullFtpPath = `ftp://${serverPath}`;
-    console.log(`${file.filename} -> ${fullFtpPath}`);
-    await exec.exec('curl', ['-s', '-T', file.filename, '--user', `${ftpUsername}:${ftpPassword}`,fullFtpPath])
-  });
-} catch (error) {
-  core.setFailed(error.message);
+    console.log('new version');
+    await Promise.all(files.map((file) => {
+      if (!file.filename.startsWith(src))
+        return;
+      const remoteFilePath = file.filename.substr(src.length);
+      const remoteDirPath = path.dirname(remoteFilePath);
+      const serverPath = `${ftpHostname}/${dest}/${remoteDirPath}/`.replace(/\/\//g, '/');
+      const fullFtpPath = `ftp://${serverPath}`;
+      console.log(`${file.filename} -> ${fullFtpPath}`);
+      return exec.exec('curl', ['-s', '-T', file.filename, '--user', `${ftpUsername}:${ftpPassword}`,fullFtpPath])
+    }));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
